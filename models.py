d290db43976fc496fbe7e1fc903d9fd13f0d7250
Adam Jenča <jenca.adam@gmail.com>
2022-02-10 14:38:31 +0100

    citatelnost

--- models.py
+++ models.py
@@ -2,8 +2,12 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine=create_engine=create_engine("sqlite:///models.db",echo=True,connect_args={"check_same_thread":False})
+engine=create_engine=create_engine("sqlite:///models.db",echo=True,connect_args={"check_same_thread":False}) # configurable
 Base = declarative_base()
+Base.metadata.create_all(engine)
+Session=sessionmaker(bind=engine)
+session=Session()
+
 class User(Base):
     __tablename__='users'
     id=Column(Integer,primary_key=True)
@@ -13,26 +17,29 @@ class User(Base):
     email=Column(String)
     pw=Column(String)
     admin=Column(Boolean)
+
 class Repo(Base):
     __tablename__='repo'
     id=Column(Integer,primary_key=True)
     owner=Column(Integer)
-    name=Column(String)
-    visible=Column(Boolean)
-    allowed=Column(String)
-Base.metadata.create_all(engine)
-Session=sessionmaker(bind=engine)
-session=Session()
+    name=Column(String) 
+    visible=Column(Boolean) #delete
+    allowed=Column(String) #delete
+
+
 def user_by_uname(un):
     return session.query(User).filter(User.uname==un)
+
 def user_by_email(em):
     return session.query(User).filter(User.email==em)
+
 def repo_by_strid(strid):
     user,name=strid.split('/')
     try:
         return session.query(Repo).filter(Repo.owner==user_by_uname(user)[0].id).filter(Repo.name==name)
     except IndexError:
         return None
+
 def repos_by_uname(uname):
     try:
         return session.query(Repo).filter(Repo.owner==user_by_uname(uname)[0].id)
